160921fe640a2f87cf83e440ec9f0aef351ee8fb
Markus Ånöstam <markus87@gmail.com>
2016-12-19 21:00:47 +0100

    Style tweaks for add-modal

--- src/app/new-person/new-person.component.ts
+++ src/app/new-person/new-person.component.ts
@@ -1,21 +1,32 @@
-import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+import {Component, Output, EventEmitter, HostBinding} from '@angular/core';
 import {Person} from '../person';
+
+const inActiveClasses = 'column is-one-third-mobile is-one-quarter-tablet';
 
 @Component({
   selector: 'f1-new-person',
   templateUrl: './new-person.component.html',
   styleUrls: ['./new-person.component.css']
 })
-export class NewPersonComponent implements OnInit {
+export class NewPersonComponent  {
   model = new Person(null, '', '', '');
 
   submitted = false;
+  isActive = false;
+
   @Output() onSubmit = new EventEmitter<Person>();
+  @HostBinding('class') classes = inActiveClasses;
+
+  activate() {
+    this.isActive = true;
+    this.classes = 'modal is-active';
+  }
 
   submit() {
     this.submitted = true;
+    this.isActive = false;
+    this.classes = inActiveClasses;
     this.onSubmit.emit(this.model);
+    this.model = new Person(null, '', '', '');
   }
-
-  ngOnInit(): void {}
 }
