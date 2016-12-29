3feb06d33406bdb92efd96e1d12702cda8dc3310
Markus Ånöstam <markus87@gmail.com>
2016-12-29 20:12:47 +0100

    Added basic name guessing feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {PeopleComponent} from './people/people.component';
-import {InMemoryPeopleDataService} from './people-data';
+import { PeopleComponent } from './people/people.component';
+import { InMemoryPeopleDataService } from './people-data';
 import { NewPersonComponent } from './new-person/new-person.component';
 import { CardComponent } from './people/card/card.component';
 import { GuessNameComponent } from './guess-name/guess-name.component';
